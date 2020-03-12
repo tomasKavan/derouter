da1d32fd997a0fe222512d726cff72ec8c8a7b34
Tomas Kavan <kavt@Tomas-MacBook-Pro.local>
2020-03-12 19:51:53 +0100

    First implementation. Added basci example. Ready for the first debugging

---
+++ example/basic/main.js
@@ -0,0 +1,33 @@
+/*!
+ * derouter
+ * Copyright(c) 2020 Tomas Kavan
+ * BSD 2-Clause Licensed
+ */
+
+/**
+ * Basic example of derouter module usage.
+ * Creates basic API server: 1 route GET /hello with 2 actions
+ * To run use command: `node ./example/basic/main.js`
+ * Application is running on the port 8083 (can be changed with
+ * `--PORT` option).
+ */
+
+// Load modules
+import express from express
+import derouter from '../../main.js'
+
+const routerDeclaration = {
+  'GET /hello/:name' : [
+    'action.today->date',
+    'action.greet:#Greetings:&.date:params.name'
+  ]
+}
+
+const app = express()
+
+app.use(derouter(routerDeclaration))
+
+const port = 8083
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
---
+++ main.js
@@ -0,0 +1,9 @@
+/*!
+ * derouter
+ * Copyright(c) 2020 Tomas Kavan
+ * BSD 2-Clause Licensed
+ */
+
+'use strict'
+
+module.exports = require('./lib/parser.js')
